a9caed7bdcdf9fa9b4a06c5896f148c90735e7b9
kannadadr <kanandar43@gmail.com>
2024-04-15 11:02:44 -0300

    test

--- src/testes/animais.test.js
+++ src/testes/animais.test.js
@@ -9,4 +9,11 @@ test('adicionarAnimal adiciona um animal à lista', () => {
 });
 
 
-  
+test('editarAnimal edita os detalhes de um animal', () => {
+    adicionarAnimal('Mia', 'Gato', 3, 'Gato preguiçoso');
+    editarAnimal(2, 'Miau', 'Gato', 4, 'Gato energético');
+    const animal = encontrarAnimalPorId(2);
+    console.log('edita os detalhes de um animal =>       ', animal);
+    expect(animal.nome).toBe('Miau');
+    expect(animal.idade).toBe(4);
+  });
